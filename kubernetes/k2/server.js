c1524f6df9f573423d9721fa0b4cf13dfaafcede
Sreeprakash Neelkantan <sree@schogini.com>
2018-07-02 12:08:06 +0530

    Minicube-Hello-Node-K2

--- kubernetes/k2/server.js
+++ kubernetes/k2/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end("Hello World!\n");
+  response.end("Hello World Again!\n");
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
